dd2d09c1ab4af3cd74c4b032f3a6f511c7697409
KarLng11 <410074645@qq.com>
2023-01-12 11:45:11 +0800

    添加 Name 字段 -MyCharacter

--- MyProject/Source/MyProject/MyCharacter.h
+++ MyProject/Source/MyProject/MyCharacter.h
@@ -26,4 +26,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(BlueprintReadWrite, Category = Attributes)
+		FName Name;
+
 };
